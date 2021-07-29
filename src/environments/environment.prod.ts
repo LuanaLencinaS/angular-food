b2d705c36f7316866ec1f463de7117322e51dc63
LuanaLencinaS <analulencina@hotmail.com>
2021-07-29 18:14:59 -0300

    feat(firebase): implementa firebase com a conta e adiciona config em environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCzxTsJt18DdRnuD4ziY2r_Sx3KV6VK-Z4",
+    authDomain: "findfood-bootcamp-angular.firebaseapp.com",
+    projectId: "findfood-bootcamp-angular",
+    storageBucket: "findfood-bootcamp-angular.appspot.com",
+    messagingSenderId: "875554592852",
+    appId: "1:875554592852:web:9ffa221bf63baca132f5c1"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCzxTsJt18DdRnuD4ziY2r_Sx3KV6VK-Z4",
+    authDomain: "findfood-bootcamp-angular.firebaseapp.com",
+    projectId: "findfood-bootcamp-angular",
+    storageBucket: "findfood-bootcamp-angular.appspot.com",
+    messagingSenderId: "875554592852",
+    appId: "1:875554592852:web:9ffa221bf63baca132f5c1"
+  },
 };
 
 /*
